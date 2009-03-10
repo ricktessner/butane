4cf58b81a489984e457aeb8a3b2261e2ea7d4b46
Rick Tessner <rick@onnadayr.ca>
2009-03-10 14:18:00 -0700

    Changed "monitor" to "sticky" in the event that I add additional filtering.

--- butane.rb
+++ butane.rb
@@ -3,7 +3,7 @@ require 'rubygems'
 require 'tinder'
 require 'yaml'
 
-def monitor_room(room, regex = nil)
+def monitor_room(room, sticky = nil)
   room.listen do |m|
     # Ignore any pings from campfire to determine if I'm still
     # here
@@ -14,8 +14,8 @@ def monitor_room(room, regex = nil)
     # If we're to monitor something in particular in this room, set the 
     # delay notification to zero which will leave the message up until
     # clicked away.
-    if regex
-      delay = 0 if m[:message] =~ /#{regex}/i
+    if sticky
+      delay = 0 if m[:message] =~ /#{sticky}/i
     end
 
     `notify-send -t #{delay} -i /home/rick/Pictures/cf.gif \"#{m[:person]}\n#{m[:message]}\"`
@@ -35,7 +35,7 @@ account_names.each do |account_name|
     rooms.keys.each do |room_name|
       room = campfire.find_room_by_name room_name
       if room
-        threads << Thread.new(room, rooms[room_name][:monitor]) do |r, rx|
+        threads << Thread.new(room, rooms[room_name][:sticky]) do |r, rx|
           monitor_room(r, rx)
         end
       end
