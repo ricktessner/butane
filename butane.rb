5a5effb03aba2c64e8bdea96417e8b7e6c827b8e
Rick Tessner <rick@onnadayr.ca>
2011-05-06 15:23:01 -0700

    Move to using EventMachine and ruby 1.9.2
    The best way to use the new butane is with rvm 1.9.2 and the bundle gem.
    
    The 'tinder' gem now uses EventMachine and this reflects part of that
    change. It looks like an individual Tinder::Room instance will fire up
    and EM reactor but what is needed here is a method to listen to multiple
    rooms.
    
    I didn't see anything like that, so I added a class method called
    listen_to_rooms to handle that. The majority of the code comes directly
    from the Tinder::Room#listen method but loops through an array of rooms
    inside of a an EM reactor loop.
    
    Threads are no longer used (well, except for the extra thread that gets
    created by EM)

--- butane.rb
+++ butane.rb
@@ -3,7 +3,59 @@ require 'rubygems'
 require 'tinder'
 require 'yaml'
 
+# Add a new class method to have the EventMachine reactor listen to multiple
+# rooms.  Basically just an implementation of the Room#listen instance method
+# from the 'tinder' gem.  This was done since each Room instance defines its
+# own EM reactor and really only want a single reactor running.
+
+module Tinder
+  class Room
+    attr_accessor :stream
+
+    def self.listen_to_rooms(rooms, options = {})
+      raise ArgumentError, "no block provided" unless block_given?
+
+      require 'twitter/json_stream'
+
+      EventMachine::run do
+        rooms.each do |room|
+          room.join # you have to be in the room to listen
+          auth = room.send(:connection).basic_auth_settings
+          room_options = {
+            :host => "streaming.#{Connection::HOST}",
+            :path => room.send(:room_url_for, :live),
+            :auth => "#{auth[:username]}:#{auth[:password]}",
+            :timeout => 6,
+            :ssl => room.send(:connection).options[:ssl]
+          }.merge(options)
+          notify "Now monitoring #{room.name}", "" # , :image => room_cfg[:image]
+          room.stream = Twitter::JSONStream.connect(room_options)
+          room.stream.each_item do |message|
+            message = HashWithIndifferentAccess.new(ActiveSupport::JSON.decode(message))
+            message[:user] = room.user(message.delete(:user_id))
+            message[:created_at] = Time.parse(message[:created_at])
+            yield(room, message)
+          end
+
+          room.stream.on_error do |message|
+            raise ListenFailed.new("got an error! #{message.inspect}!")
+          end
+
+          room.stream.on_max_reconnects do |timeout, retries|
+            raise ListenFailed.new("Tried #{retries} times to connect. Got disconnected from #{@name}!")
+          end
+
+          # if we really get disconnected
+          raise ListenFailed.new("got disconnected from #{@name}!") if !EventMachine.reactor_running?
+        end
+      end
+    end
+  end
+end
+
 def notify(title, message = "", options = {})
+  return if message.nil?
+
   img_opt = "-i #{options[:image]}" if options[:image]
   delay_opt = ""
   if options[:delay]
@@ -23,47 +75,20 @@ def notify(title, message = "", options = {})
   # in the msg.  Assumes that in href=stuff, stuff has no whitespace.
   msg.gsub! /<a[^>]+(href=[^(\s|>)]+)[^>]*>/, '<a \1>'
 
-  `notify-send #{delay_opt} #{img_opt} \"#{title}\" '#{msg}'`
-end
-
-def monitor_room(room, config = {})
-  sticky = config[:sticky]
-  ignore = config[:ignore]
-
-  room_name = room.name.gsub /"/, '' # Get rid of any dquotes since we use 'em to delimit person
-
-  last_message_id = 0
-  room.listen do |m|
-    # Ignore any pings from campfire to determine if I'm still
-    # here
-    next if !m[:user] || m[:user][:name].strip.empty?  # Ignore anything from a nil / empty person
-
-    next if m[:id].to_i <= last_message_id
-    last_message_id = m[:id].to_i
-
-    delay = 5000 # in milliseconds (time to display the notification)
-
-    # If we're to monitor something in particular in this room, set the 
-    # delay notification to zero which will leave the message up until
-    # clicked away.
-    if sticky
-      delay = 0 if m[:body] =~ /#{sticky}/i
-    end
-
-    if ignore
-      next if m[:body] =~ /#{ignore}/
-    end
-
-    person = m[:user][:name].gsub /"/, ''  # Get rid of any dquotes since we use 'em to delimit person
-
-    notify("#{person} in #{room_name}", m[:body], :delay => delay, :image => config[:image])
-  end
+  `notify-send #{delay_opt} #{img_opt} \\"#{title}\\" '#{msg}'`
+  puts "title: #{title}"
+  puts "msg: #{msg}"
 end
 
 config = YAML::load_file("#{ENV['HOME']}/.butanerc")
 account_names = config.keys
 
-threads = []
+tinder_rooms = []
+room_configs = {}
+
+# Build up an array of Tinder::Room and a hash of ignore/sticky/image options
+# indexed by the name of each room for use in the block passed to the new
+# listen_to_rooms() method.
 account_names.each do |account_name|
   rooms = config[account_name][:rooms]
   account_img = config[account_name][:image]
@@ -84,16 +109,12 @@ account_names.each do |account_name|
     end
     notify "Successfully logged in to #{account_name}", "", :image => account_img
 
-    # Start up a thread for each room we are going to monitor
     rooms.keys.each do |room_name|
       room = campfire.find_room_by_name room_name
       if room
-        room_cfg = rooms[room_name] || {}
-        room_cfg[:image] ||= account_img
-        notify "Now monitoring #{room_name}", "", :image => room_cfg[:image]
-        threads << Thread.new(room, room_cfg) do |r, cfg|
-          monitor_room(r, cfg)
-        end
+        room_configs[room.name] = rooms[room.name] || {}
+        room_configs[room.name][:image] ||= account_img
+        tinder_rooms << room
       else
         notify "Did not find #{room_name}, not monitoring"
       end
@@ -101,4 +122,32 @@ account_names.each do |account_name|
   end
 end
 
-threads.each { |t| t.join }
+last_message_id = Hash.new(0)
+
+Tinder::Room.listen_to_rooms(tinder_rooms) do |tinder_room, m|
+  next if !m[:user] || m[:user][:name].strip.empty?  # Ignore anything from a nil / empty person
+
+  next if m[:id].to_i <= last_message_id[tinder_room.id]
+  last_message_id[tinder_room.id] = m[:id].to_i
+
+  delay = 5000 # in milliseconds (time to display the notification)
+
+  sticky = room_configs[tinder_room.name][:sticky]
+  ignore = room_configs[tinder_room.name][:ignore]
+  image  = room_configs[tinder_room.name][:image]
+
+  # If we're to monitor something in particular in this room, set the
+  # delay notification to zero which will leave the message up until
+  # clicked away.
+  if sticky
+    delay = 0 if m[:body] =~ /#{sticky}/i
+  end
+
+  if ignore
+    next if m[:body] =~ /#{ignore}/
+  end
+
+  person = m[:user][:name].gsub /"/, ''  # Get rid of any dquotes since we use 'em to delimit person
+
+  notify("#{person} in #{tinder_room.name}", m[:body], :delay => delay, :image => image)
+end
